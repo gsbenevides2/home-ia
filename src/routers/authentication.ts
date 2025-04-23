81c355899fa7ee014a0a038a2b6183f8b8010faf
Guilherme da Silva Benevides <git@gui.dev.br>
2025-04-23 09:59:12 -0300

    fix: MCP Auth For Cursor

--- src/routers/authentication.ts
+++ src/routers/authentication.ts
@@ -1,7 +1,26 @@
 import { type NextFunction, type Request, type Response, Router } from 'express'
+import { transports } from '../mcp/server'
 const authenticationRouter = Router()
 
 authenticationRouter.use((req: Request, res: Response, next: NextFunction) => {
+  const token = req.query.token as string
+
+  if (req.path === '/sse' && req.query.token === Bun.env.AUTH_TOKEN) {
+    next()
+    return
+  } else if (
+    req.path === '/messages' &&
+    Object.keys(transports).includes(req.query.sessionId as string)
+  ) {
+    next()
+    return
+  }
+
+  console.log(req.query)
+  if (token === Bun.env.AUTH_TOKEN) {
+    next()
+    return
+  }
   const authorizationHeader = req.headers.authorization
   if (!authorizationHeader) {
     return res.status(401).json({ message: 'Unauthorized' })
