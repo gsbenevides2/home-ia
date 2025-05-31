75cc79003dcf0d86e35ec23739365032a78e5c7d
Guilherme da Silva Benevides <git@gui.dev.br>
2025-05-31 14:12:24 -0300

    feat: add depth and recursive fetching options to MarkdownfyWebpage for enhanced webpage conversion

--- src/mcp/tool/markdownfy/webpage.ts
+++ src/mcp/tool/markdownfy/webpage.ts
@@ -5,7 +5,23 @@ import { z } from 'zod'
 import { AbstractTool, type OnErrorToolCallback } from '../AbstractTool'
 
 const args = {
-  url: z.string().url().describe('URL of the webpage to convert')
+  url: z.string().url().describe('URL of the webpage to convert'),
+  depth: z
+    .number()
+    .min(0)
+    .max(3)
+    .default(0)
+    .optional()
+    .describe(
+      'Used to access the subpages of the webpage, set the depth, default is 0 (Not Subpages) and max is 3 (Main page -> Subpages -> Subpages -> Subpages)'
+    ),
+  allowRecusiveForSubpages: z
+    .boolean()
+    .default(true)
+    .optional()
+    .describe(
+      'If true, the tool will access the subpages of this page, default is true'
+    )
 } as const
 
 type Args = typeof args
@@ -31,6 +47,32 @@ export class MarkdownfyWebpage extends AbstractTool<Args> {
     const html = response.data
 
     const markdown = NodeHtmlMarkdown.translate(html)
-    return { content: [{ type: 'text', text: markdown }] }
+    let markdResult = markdown
+    const urls = markdown.match(/https?:\/\/[^\s]+/g)
+    const currentDepth = 0
+    while (currentDepth < (args.depth ?? 0)) {
+      if (!urls) {
+        break
+      }
+      while (urls.length > 0) {
+        const url = urls.shift()
+        if (!url) {
+          break
+        }
+        if (args.allowRecusiveForSubpages && !url.startsWith(url)) {
+          continue
+        }
+
+        const response = await axios.get<string>(url)
+        const html = response.data
+        const markdown = NodeHtmlMarkdown.translate(html)
+        markdResult += markdown
+        const newUrls = markdown.match(/https?:\/\/[^\s]+/g)
+        if (newUrls) {
+          urls.push(...newUrls)
+        }
+      }
+    }
+    return { content: [{ type: 'text', text: markdResult }] }
   }
 }
